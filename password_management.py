0bbbfa47d583ce4951a08a1aea0a14eb50c1ac1b
Joacodef <joacodef1@gmail.com>
2023-09-03 16:53:52 -0300

    Ahora se usa user_password, y funciona todo

--- password_management.py
+++ password_management.py
@@ -1,99 +1,126 @@
 import os
 from cryptography.fernet import Fernet
 import base64
+import bcrypt
 
-def create_master_key():
-    file_name = 'secret.txt'
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    master_key = base64.urlsafe_b64encode(os.urandom(32))
-    with open(file_name, "wb") as keyfile:
-        keyfile.write(master_key)
-    return master_key
-    
-def get_master_key():
-    file_name = 'secret.txt'
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    if os.path.isfile(file_name):
-        with open(file_name, "rb") as keyfile:
-            master_key = keyfile.read()
-            return master_key
+def store_user_password(password):
+    if not os.path.isfile('user_key.txt'):
+        with open('user_key.txt', 'wb') as f:
+            pass
     else:
-        master_key = create_master_key()
-    return master_key
+        os.remove('user_key.txt')
+        with open('user_key.txt', 'wb') as f:
+            pass
+    # store the hashed password in the file
+    with open('user_key.txt', 'ab') as f:
+        hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+        f.write(hashed_password)
+    
+    return 'Password stored'
 
-def encrypt(word):
-    master_key = get_master_key()
-    fernet = Fernet(master_key)
+
+def validate_user_password(password):
+    if not os.path.isfile('user_key.txt'):
+        with open('user_key.txt', 'wb') as f:
+            pass
+    with open('user_key.txt', 'rb') as f:
+        for line in f:
+            hashed_password = line
+            if bcrypt.checkpw(password.encode(), hashed_password):
+                return True
+    return False
+
+
+def encrypt(word, master_key):
+    fernet = Fernet(base64.urlsafe_b64encode(master_key.encode().ljust(32)))
     encrypted_word = fernet.encrypt(word.encode())
     return encrypted_word
 
-def decrypt(word):
-    master_key = get_master_key()
-    fernet = Fernet(master_key)
+
+def decrypt(word, master_key):
+    fernet = Fernet(base64.urlsafe_b64encode(master_key.encode().ljust(32)))
     decrypted_word = fernet.decrypt(word).decode()
     return decrypted_word
 
 
-def get_information(word):
-    # if the file passwords.txt doesn't exist, create it
+def get_information(word, master_key):
+    if validate_user_password(master_key) == False:
+        return 'Wrong password'
     if not os.path.isfile('passwords.txt'):
         with open('passwords.txt', 'wb') as f:
             pass
     with open('passwords.txt', 'rb') as f:
         for line in f:
-            decrypted_data = decrypt(line)
+            decrypted_data = decrypt(line, master_key)
             decrypted_data = decrypted_data.split('_')
             if decrypted_data[2] == word or decrypted_data[0] == word:
                 return decrypted_data[0] + ' ' + decrypted_data[1]
-    return 'Keyword not found'
+    return 'Not found'
 
 
-def store_information(account, password, keyword):
+def store_information(account, password, keyword, master_key):
+    if validate_user_password(master_key) == False:
+        return 'Wrong password'
     if not os.path.isfile('passwords.txt'):
         with open('passwords.txt', 'wb') as f:
             pass
     with open('passwords.txt', 'rb') as f:
         for line in f:
-            decrypted_data = decrypt(line)
+            decrypted_data = decrypt(line, master_key)
             decrypted_data = decrypted_data.split('_')
             if decrypted_data[2] == keyword or decrypted_data[0] == account:
                 return 'Keyword or account already exists' # esto deberia ser un error
     # if not, store information
     with open('passwords.txt', 'ab') as f:
-        encrypted_data = encrypt(account + '_' + password + '_' + keyword)
+        encrypted_data = encrypt(account + '_' + password + '_' + keyword, master_key)
         f.write(encrypted_data + b'\n')
     return 'Information stored'
 
         
-def update_information(word, new_password):
+def update_information(word, new_password, master_key):
+    if validate_user_password(master_key) == False:
+        return 'Wrong password'
     if not os.path.isfile('passwords.txt'):
         with open('passwords.txt', 'wb') as f:
             pass
     with open('passwords.txt', 'rb') as f:
         for line in f:
-            decrypted_data = decrypt(line)
+            decrypted_data = decrypt(line, master_key)
             decrypted_data = decrypted_data.split('_')
             if decrypted_data[2] == word or decrypted_data[0] == word:
                 account = decrypted_data[0]
                 keyword = decrypted_data[2]
-                store_information(account, new_password, keyword)
+                print(delete_information(word, master_key))
+                print(store_information(account, new_password, keyword, master_key))
                 return 'Information updated'
     return 'Keyword or Account not found' # esto deberia ser un error
 
-def delete_information(word):
+def delete_information(word, master_key):
+    lista_aux = []
+    flag_found = False
+    if validate_user_password(master_key) == False:
+        return 'Wrong password'
     if not os.path.isfile('passwords.txt'):
         with open('passwords.txt', 'wb') as f:
             pass
     with open('passwords.txt', 'rb') as f:
         for line in f:
-            decrypted_data = decrypt(line)
+            decrypted_data = decrypt(line, master_key)
             decrypted_data = decrypted_data.split('_')
-            if decrypted_data[2] == word or decrypted_data[0] == word:
-                line = line.replace(line, b'')
-                return 'Information deleted'
-    return 'Keyword or Account not found' # esto deberia ser un error
+            if decrypted_data[2] != word and decrypted_data[0] != word:
+                lista_aux.append(line)
+            else:
+                flag_found = True
+    if flag_found == False:
+        return 'Keyword or Account not found'
+    with open('passwords.txt', 'wb') as f:
+        for line in lista_aux:
+            f.write(line)  
+    return 'Information deleted'
 
-store_information('facebook', '123456', 'face')
-store_information('instagram', '123456', 'insta')
-store_information('twitter', '123456', 'X')
+
+#print(store_information('facebook', '123456', 'face', '123456'))
+#store_information('instagram', '123456', 'insta')
+#store_information('twitter', '123456', 'X')
 #print(get_information('twitter'))
+#print(get_information('face', '123456'))
--- passwords.py
+++ passwords.py
@@ -1,6 +1,28 @@
 import password_management
+import os
 
 fin = False #por si acaso se sale en otro lado
+password = ""
+
+if not os.path.isfile('user_key.txt'):
+    print("Bienvenido a Password Manager. Debes crear una contraseña para continuar")
+    while True:
+        password = input("Ingresa una contraseña: ")
+        password_confirmation = input("Confirma tu contraseña: ")
+        if password == password_confirmation:
+            print(password_management.store_user_password(password))
+            break
+        else:
+            print("Las contraseñas no coinciden. Intenta de nuevo")
+
+    print("Bienvenido a Password Manager. Ingresa tu contraseña para continuar")
+
+while True:
+    password = input("Ingresa tu contraseña: ")
+    if password_management.validate_user_password(password):
+        break
+    else:
+        print("Contraseña incorrecta. Intenta de nuevo")
 
 while not fin:
     comando = input("Ingresa un comando (agregar, borrar, actualizar, recuperar o fin): ")
@@ -9,24 +31,27 @@ while not fin:
         break
     elif comando.lower() == "agregar":
         print("Agregando...")
-        nombre = input("Ingresa el login o keyword de la cuenta (email o username): ")
+        nombre = input("Ingresa el login de la cuenta (email o username): ")
         contraseña = input("Ingresa la contraseña: ")
         palabra_clave = input("Ingresa una palabra clave (enter para dejar vacio): ")
         if palabra_clave == "":
             palabra_clave = None
-        print(password_management.store_information(nombre, contraseña, palabra_clave))
-        print("Cuenta agregada exitosamente!")
+        if password_management.store_information(nombre, contraseña, palabra_clave, password)== "Keyword or account already exists":
+            print("La cuenta ya existe")
+        else:
+            print("Cuenta agregada exitosamente!")
     elif comando.lower() == "borrar":
         print("Borrando...")
         nombre = input("Ingresa el login o keyword de la cuenta a borrar: ") #o palabra clave?
-        print(password_management.delete_information(nombre))
+        print(password_management.delete_information(nombre, password))
     elif comando.lower() == "actualizar":
         print("Actualizando...")
         nombre = input("Ingresa el login o keyword de la cuenta a actualizar: ")
-        print(password_management.update_information(nombre))
+        new_pass = input("Ingresa la nueva contraseña: ")
+        print(password_management.update_information(nombre, new_pass, password))
     elif comando.lower() == "recuperar":
         print("Recuperando...")
         nombre = input("Ingresa el login o keyword de la cuenta a recuperar: ")
-        print(password_management.get_information(nombre))
+        print(password_management.get_information(nombre, password))
     else:
         print("Comando no reconocido")
